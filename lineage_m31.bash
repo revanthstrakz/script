117569438f00e5dec76f9a41588a3f366039ab83
Revanth Strakz <dr.revanthstrakz@gmail.com>
2020-09-19 20:37:40 +0530

    Update lineage_m31.bash

--- lineage_m31.bash
+++ lineage_m31.bash
@@ -22,13 +22,14 @@ export LC_ALL=C
 ccache -s
 
 cd $ROOTDIR
+cd ~/android/lineage
 
 echo '[+] Fetch vendor trees...'
 if [ -d vendor/samsung ]; then
     git -C vendor/samsung fetch origin
     git -C vendor/samsung reset --hard origin/lineage-17.1
 else
-	git clone https://github.com/erfanoabdi/proprietary_vendor_samsung -b lineage-17.1 vendor/samsung
+	git clone --depth=1 https://github.com/erfanoabdi/proprietary_vendor_samsung -b lineage-17.1 vendor/samsung
 fi
 
 #rm -rf device/samsung/m31
@@ -36,7 +37,7 @@ if [ -d device/samsung/m31 ]; then
     git -C device/samsung/m31 fetch origin
     git -C device/samsung/m31 reset --hard origin/lineage-17.1
 else
-	git clone https://github.com/erfanoabdi/android_device_samsung_m31 -b lineage-17.1 device/samsung/m31
+	git clone --depth=1 https://github.com/erfanoabdi/android_device_samsung_m31 -b lineage-17.1 device/samsung/m31
 fi
 
 #rm -rf device/samsung/universal9610-common
@@ -44,7 +45,7 @@ if [ -d device/samsung/universal9610-common ]; then
     git -C device/samsung/universal9610-common fetch origin
     git -C device/samsung/universal9610-common reset --hard origin/lineage-17.1
 else
-	git clone https://github.com/erfanoabdi/android_device_samsung_universal9610-common -b lineage-17.1 device/samsung/universal9610-common
+	git clone --depth=1 https://github.com/erfanoabdi/android_device_samsung_universal9610-common -b lineage-17.1 device/samsung/universal9610-common
 fi
 
 #rm -rf kernel/samsung/universal9610
@@ -52,9 +53,10 @@ if [ -d kernel/samsung/universal9610 ]; then
     git -C kernel/samsung/universal9610 fetch origin
     git -C kernel/samsung/universal9610 reset --hard origin/lineage-17.1_older
 else
-	git clone https://github.com/erfanoabdi/android_kernel_samsung_universal9610 -b lineage-17.1_older kernel/samsung/universal9610
+	git clone --depth=1 https://github.com/erfanoabdi/android_kernel_samsung_universal9610 -b lineage-17.1_older kernel/samsung/universal9610
 fi
 
+cd ~/android/lineage
 echo '[+] Setup environment...'
 . build/envsetup.sh
 
