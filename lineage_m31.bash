6581c66f6cd8041a4d402b52c8096178de4697dd
Revanth Strakz <dr.revanthstrakz@gmail.com>
2020-10-27 19:05:12 +0530

    Update lineage_m31.bash

--- lineage_m31.bash
+++ lineage_m31.bash
@@ -57,9 +57,9 @@ ls -al
 echo '[+] Setup environment...'
 . build/envsetup.sh
 
-#sudo dd if=/dev/zero of=/mnt/swapfile bs=1M count=18000
-#sudo mkswap /mnt/swapfile
-#sudo swapon /mnt/swapfile
+sudo dd if=/dev/zero of=/mnt/swapfile bs=1M count=10000
+sudo mkswap /mnt/swapfile
+sudo swapon /mnt/swapfile
 
 
 echo '[+] Lunching...'
