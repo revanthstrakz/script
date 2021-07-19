3b79f027e88b813666b1e8e7eb978f0ce8b0e67d
Thopucherla Revanth Rao <dr.revanthstrakz@gmail.com>
2021-07-19 23:50:51 +0530

    Update giter.sh

--- giter.sh
+++ giter.sh
@@ -1,12 +1,12 @@
-$l=0
+l=0
 for i in *
 do 
     git add $i 
     git commit -m "added $i"
-    (($l++))
+    ((l++))
     if [[ $l -eq 5 ]]; then
         git push
-        $l=0
+        l=0
     fi
         
 done
